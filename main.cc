107bdeb1d2ff7d862054bf77f7c6b6edeba50539
Gokul Selvan <mgokulselvan@gmail.com>
2026-09-13 00:03:17 +0530

    High Rez High Sampling different materials

--- main.cc
+++ main.cc
@@ -24,9 +24,9 @@ int main(){
 	camera cam;
 
 	cam.aspect_ratio = 16.0 / 9.0;
-	cam.image_width = 400;
-	cam.samples_per_pixel = 100;
-	cam.max_depth = 50;
+	cam.image_width = 1920;
+	cam.samples_per_pixel = 1000;
+	cam.max_depth = 40;
 
 	cam.render(world);
 }
