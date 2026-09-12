e7f3925bc6edfe720b74a574b06040ac3e5a6686
Gokul Selvan <mgokulselvan@gmail.com>
2026-09-12 12:17:44 +0530

    gamma correction

--- color.h
+++ color.h
@@ -7,6 +7,7 @@
 
 using color = vec3;
 
+//images store colors in gamma space, some weird space, but what we are calculating is linear space, so we are using this function to just uk, map or transform whatever physically accurate mathematically accurate colors we got here to the gamma space, why? even tho the light intensity and color part are correct!, its because thats how stupid computers work, they are sassy and want it in their own preferred format, to accurately display that we actually want to display and what the physics and maths actually result to, its just that we are converting it form one unit to another, we arent modifying anything, just making it computer compatible from physics and math compatible 
 inline double linear_to_gamma(double linear_component){
 	if(linear_component > 0)
 		return std::sqrt(linear_component);
