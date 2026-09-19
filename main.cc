75e84f373bb47a706f56125f22d28fd55a214b7b
Gokul Selvan <mgokulselvan@gmail.com>
2026-09-19 12:52:55 +0530

    configurable param for noise frequency
    idk anything about perlin noise , why are we doing this

--- main.cc
+++ main.cc
@@ -119,7 +119,7 @@ void earth() {
 
 void perlin_spheres() {
 	hittable_list world;
-	auto pertext = make_shared<noise_texture>();
+	auto pertext = make_shared<noise_texture>(5);
 	world.add(make_shared<sphere>(point3(0,-1000,0), 1000, make_shared<lambertian>(pertext)));
 	world.add(make_shared<sphere>(point3(0,2,0), 2, make_shared<lambertian>(pertext)));
 
